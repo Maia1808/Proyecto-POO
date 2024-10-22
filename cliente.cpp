faaddf1a861ebe8c5b39976ead1aaeac986f8f38
Santiago <santiagotisera866@gmail.com>
2024-10-22 15:58:48 -0300

    Actualización

--- cliente.cpp
+++ cliente.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-#include "libreria_Chris_Morley/XmlRpc.h"
+#include "libreria_Chris_Morley/XmlRpc.h" 
 using namespace XmlRpc;
+
+void mostrarMenu() {
+    cout << "Menu de opciones:\n";
+    cout << "1. Saludo personalizado\n";
+    cout << "2. Salir y apagar todo\n";
+    cout << "Seleccione una opción: ";
+}
 
 int main(int argc, char* argv[])
 {
@@ -12,21 +19,41 @@ int main(int argc, char* argv[])
   }
   
   int port = atoi(argv[2]);
-  
-  // Crear el cliente XML-RPC y conectarse al servidor
   XmlRpcClient client(argv[1], port);
+  XmlRpcValue result, noArgs, args;
+
+  int opcion;
   
-  XmlRpcValue noArgs, result;
+  while (true) {
+    mostrarMenu();
+    cin >> opcion;
 
-  // Llamar al método 'saludo' en el servidor
-  if (client.execute("saludo", noArgs, result))
-    std::cout << "Respuesta del servidor: " << result << "\n\n";
-  else
-    std::cerr << "Error al ejecutar el método 'saludo'\n\n";
+    if (opcion == 1) {
+      // Solicitar saludo personalizado
+      string nombre;
+      cout << "Ingrese su nombre para el saludo personalizado: ";
+      cin >> nombre;
+      args[0] = nombre;
 
-  char salida;
-  cout << "Ingrese cualquier caracter para salir...";
-  cin >> salida;
+      if (client.execute("saludo_personalizado", args, result))
+        std::cout << "Respuesta del servidor: " << result << "\n\n";
+      else
+        std::cerr << "Error al ejecutar el saludo personalizado\n\n";
+    }
+    else if (opcion == 2) {
+      // Apagar el servidor y salir
+      if (client.execute("apagar_servidor", noArgs, result))
+        std::cout << "Servidor apagado correctamente.\n\n";
+      else
+        std::cerr << "Error al intentar apagar el servidor.\n\n";
+
+      cout << "Cliente apagado.\n";
+      break;
+    }
+    else {
+      cout << "Opción inválida. Intente de nuevo.\n";
+    }
+  }
 
   return 0;
 }
--- servidorPrueba.py
+++ servidorPrueba.py
@@ -1,5 +1,6 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
+import sys
 
 # Restringir a un subdirectorio /RPC2
 class RequestHandler(SimpleXMLRPCRequestHandler):
@@ -9,12 +10,19 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
 with SimpleXMLRPCServer(("127.0.0.1", 8080), requestHandler=RequestHandler) as server:
     server.register_introspection_functions()
 
-    # Definir una función remota que el cliente podrá invocar
-    def saludo():
-        return "¡Conexión exitosa con el servidor XML-RPC!"
+    # Definir una función remota para el saludo personalizado
+    def saludo_personalizado(nombre):
+        return f"Hola {nombre}, ¡conexión exitosa con el servidor XML-RPC!"
 
-    # Registrar la función con el servidor
-    server.register_function(saludo, "saludo")
+    # Función para apagar el servidor
+    def apagar_servidor():
+        print("El servidor se está apagando...")
+        server.server_close()
+        sys.exit()
+
+    # Registrar las funciones con el servidor
+    server.register_function(saludo_personalizado, "saludo_personalizado")
+    server.register_function(apagar_servidor, "apagar_servidor")
 
     # Iniciar el servidor
     print("Servidor XML-RPC escuchando en 127.0.0.1:8080...")
