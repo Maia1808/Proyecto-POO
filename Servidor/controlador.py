69c72ef21e785885d05ab97c92b0fb65a3fecbe1
Marcos-Guerreiro <marcos2003gh@gmail.com>
2024-10-23 21:22:55 -0300

    TEst PR

--- Servidor/controlador.py
+++ Servidor/controlador.py
@@ -2,21 +2,25 @@ import serial
 import time
 import logTrabajo
 
+##
+##ESte es un comment para generar un PR
+
+
 class Controlador:
 
     def __init__(self):
         self.estado_robot = False
         self.estado_motores = False
         self.baudrate = 9600
-        self.puerto_COM = 'COM3'
+        self.puerto_COM = "COM3"
         self.arduino = None
 
     def get_estado_robot(self):
         return self.estado_robot
-    
+
     def get_estado_motores(self):
         return self.estado_motores
-    
+
     def cambiar_parametros_comunicacion(self, baudrate, puerto_COM):
         # Cerrar la conexión actual si está activa antes de cambiar los parámetros
         if self.estado_robot:
@@ -24,15 +28,21 @@ class Controlador:
 
         self.baudrate = baudrate
         self.puerto_COM = puerto_COM
-        print(f"Parámetros de comunicación cambiados: Baudrate={self.baudrate}, Puerto={self.puerto_COM}")
+        print(
+            f"Parámetros de comunicación cambiados: Baudrate={self.baudrate}, Puerto={self.puerto_COM}"
+        )
 
     def conectar_robot(self):
         try:
             self.arduino = serial.Serial(self.puerto_COM, self.baudrate, timeout=1)
             self.estado_robot = True
-            print(f"Conexión establecida en {self.puerto_COM} con baudrate {self.baudrate}.")
+            print(
+                f"Conexión establecida en {self.puerto_COM} con baudrate {self.baudrate}."
+            )
         except serial.SerialException as e:
-            print(f"Error al conectar: Verifique que el puerto {self.puerto_COM} esté disponible y correcto.")
+            print(
+                f"Error al conectar: Verifique que el puerto {self.puerto_COM} esté disponible y correcto."
+            )
         except Exception as e:
             print(f"Error al conectar: {e}")
 
@@ -46,7 +56,7 @@ class Controlador:
 
     def activar_motores(self):
         if self.estado_robot:
-            respuesta = self.enviar_comando('M17')
+            respuesta = self.enviar_comando("M17")
             if respuesta:
                 print("Motores activados.")
                 self.estado_motores = True
@@ -54,10 +64,10 @@ class Controlador:
                 print("Error al activar motores.")
         else:
             print("No se pueden activar los motores. El robot no está conectado.")
-    
+
     def desactivar_motores(self):
         if self.estado_robot:
-            respuesta = self.enviar_comando('M18')
+            respuesta = self.enviar_comando("M18")
             if respuesta:
                 print("Motores desactivados.")
                 self.estado_motores = False
@@ -69,9 +79,13 @@ class Controlador:
     def enviar_comando(self, comando):
         if self.estado_robot:
             try:
-                self.arduino.write(comando.encode())  # Enviar comando en formato de bytes
+                self.arduino.write(
+                    comando.encode()
+                )  # Enviar comando en formato de bytes
                 time.sleep(1)  # Tiempo de espera para recibir respuesta
-                respuesta = self.arduino.readline().decode('utf-8').strip()  # Leer respuesta
+                respuesta = (
+                    self.arduino.readline().decode("utf-8").strip()
+                )  # Leer respuesta
                 if respuesta:
                     print(f"Respuesta recibida: {respuesta}")
                 else:
